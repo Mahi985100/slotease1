6e7f385c3ef1503e23150ab35113aca884956756
3144_Mahi Nai <parekhmahi36@gmail.com>
2026-06-24 17:56:09 +0530

    Update views.py

--- tenants/views.py
+++ tenants/views.py
@@ -2,3 +2,12 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("SlotEase is running!")
+
+def landing(request):
+    return HttpResponse("Welcome to SlotEase")
+
+def register(request):
+    return HttpResponse("Register Page")
+
+def staff_login_view(request):
+    return HttpResponse("Staff Login Page")
